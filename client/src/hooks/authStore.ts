9f473cf24f4822c7dbccd5b200d605522dbcec5d
RainZerg <valeryy89@gmail.com>
2025-02-26 12:52:35 +0300

    Now authStore is storing in sessionStorage instead of localStorage

--- client/src/hooks/authStore.ts
+++ client/src/hooks/authStore.ts
@@ -1,5 +1,5 @@
 import {create} from 'zustand';
-import {persist} from 'zustand/middleware';
+import {persist, createJSONStorage} from 'zustand/middleware';
 
 type AuthState = {
     token: string | null;
@@ -20,6 +20,7 @@ export const useAuthStore = create<AuthState>()(
         }),
         {
             name: 'auth-storage',
+            storage: createJSONStorage(() => sessionStorage),
         }
     )
 );
